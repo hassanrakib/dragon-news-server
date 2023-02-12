fc2bbc3e111d55e7c57a1e029153e1a5eac05be2
Rakib Hassan <mailrakibhassan@gmail.com>
2023-02-12 23:26:29 +0600

    multiple api created

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 // get news categories
 const categories = require("./data/categories.json");
 
+// get news
+const news = require("./data/news.json");
+
 const app = express();
 app.use(cors());
 const port = 5000;
@@ -14,6 +17,31 @@ app.get("/", (req, res) => {
 // categories api
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+// get all news
+app.get("/news", (req, res) => {
+  res.send(news);
+})
+
+// get multiple news by category
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const newsByCategory = news.filter(
+      (singleNews) => singleNews.category_id === id
+    );
+    res.send(newsByCategory);
+  }
+});
+
+// get single news
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((singleNews) => singleNews._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
