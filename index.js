d79ee5ed2e2bab7c0466438806c42e581fcf6a04
Rakib Hassan <mailrakibhassan@gmail.com>
2023-02-12 20:11:17 +0600

    news-categories api created

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+// get news categories
+const categories = require("./data/categories.json");
+
 const app = express();
 app.use(cors());
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
+});
+
+// categories api
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
 });
 
 app.listen(port, () => {
